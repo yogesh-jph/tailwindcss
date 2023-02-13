ab2aba8ba1ab1e73197a0b8318ced09ed49e9a52
Gaurav Kumar <gauravku1586@gmail.com>
2023-02-13 12:27:35 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import './components/navbar'
+import Navbar from './components/navbar'
+import Footer from './components/footer'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <Navbar>
+        
+      </Navbar>
+      <h1><b>HELLO YOGESH</b></h1>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/footer.jsx
@@ -0,0 +1,5 @@
+
+export default function Footer(){
+    return <footer><h1>this is footer</h1></footer>
+    
+}
